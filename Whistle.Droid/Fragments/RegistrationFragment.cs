c87d40c441ee69b0a6637ad6c32ae38d10f594af
RIYAZ <rzee.m7@gmail.com>
2014-08-30 17:19:37 +0530

    Merge code

--- Whistle.Droid/Fragments/RegistrationFragment.cs
+++ Whistle.Droid/Fragments/RegistrationFragment.cs
@@ -1,15 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Support.V4.App;
-using Android.Util;
 using Android.Views;
-using Android.Widget;
+using Cirrious.MvvmCross.Binding.Droid.BindingContext;
 using Cirrious.MvvmCross.Droid.Fragging.Fragments;
 
 namespace Whistle.Droid.Fragments
@@ -27,7 +18,9 @@ namespace Whistle.Droid.Fragments
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            return inflater.Inflate(Resource.Layout.Registration, container, false);
+            base.OnCreateView(inflater, null, savedInstanceState);
+            //return inflater.Inflate(Resource.Layout.Registration, container, false);
+            return this.BindingInflate(Resource.Layout.Registration, null);
         }
 
         #endregion

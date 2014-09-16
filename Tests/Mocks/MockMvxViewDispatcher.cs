a8208c33bd304608def08b5ccc69d524f21a508e
talanta <a.talviy@gmail.com>
2014-09-16 22:01:02 +0200

    add a test project.

---
+++ Tests/Mocks/MockMvxViewDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using Cirrious.CrossCore.Core;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Mocks
+{
+    public class MockMvxViewDispatcher : MvxMainThreadDispatcher, IMvxViewDispatcher
+    {
+        public List<IMvxViewModel> CloseRequests = new List<IMvxViewModel>();
+        public List<MvxViewModelRequest> NavigateRequests = new List<MvxViewModelRequest>();
+
+        public bool ShowViewModel(MvxViewModelRequest request)
+        {
+            NavigateRequests.Add(request);
+            return true;
+        }
+
+        public bool ChangePresentation(MvxPresentationHint hint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RequestMainThreadAction(Action action)
+        {
+            action();
+            return true;
+        }
+    }
+}
